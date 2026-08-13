9e82cd968ee7d34d8c935bb6cddf09ce13e5ebcf
camilo525 <camilo@thrust-aviation.com>
2026-08-13 14:49:30 -0400

    Change default timezone from UTC to Local

--- app.py
+++ app.py
@@ -62,11 +62,11 @@ def get_airport_details(icao):
             city = stn.get("city", "N/A")
             state = stn.get("state", "N/A")
             # Extraemos solo el ID de la zona horaria (ej: America/New_York)
-            tz = stn.get("timezone", {}).get("tzid", "UTC")
+            tz = stn.get("timezone", {}).get("tzid", "Local")
             return [name, city, state, tz]
     except:
         pass
-    return [icao, "Unknown City", "Unknown State", "UTC"]
+    return [icao, "Unknown City", "Unknown State", "Local"]
 
 WEATHER_ICONS = {"Sunny": "☼", "Partly Cloudy": "☁", "Cloudy": "☁", "Rainy": "☂", "Thunderstorm": "⚡", "Snowy": "❄", "Foggy": "░"}
 
